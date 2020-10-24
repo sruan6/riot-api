351c59554c353211a9e9bfd24807c2e51dd6d92a
David <davidhuytran@live.com>
2020-10-24 01:24:14 -0700

    Added files for clash v3

---
+++ example/lol_examples/clash_example/sample.js
@@ -0,0 +1,10 @@
+const { Lol } = require('../../..');
+
+const lol = new Lol({ region: 'NA', apikey: 'YOUR API KEY' });
+const accId = "Account Id";
+
+const getPlayersByAccId = (id) => {
+    
+}
+
+getPlayersByAccId(accId);
---
+++ src/lol/clashV3/index.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+// CLASH-V3
+const Clash = function Clash(region, apikey) {
+    this.playersByAccId = async (AccId) => {
+        const {data} = await axios.get(
+            `https://${region[0]}/lol/clash/v1/players/by-summoner/${AccId}?api_key=${apikey}`
+        );
+        return data;
+    };
+}
+
+module.exports = Clash;
