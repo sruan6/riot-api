ef15c43698b763984f94c11bf0f394aa0788ea42
ruan060606 <ruan060606@yahoo.com>
2020-10-24 04:26:22 -0700

    list

--- index.js
+++ index.js
@@ -4,10 +4,4 @@
 // const Lol = require('./dist/lol');
 // const Tft = require('./dist/tft');
 
-const Riot = require('./src/riot');
-const Val = require('./src/val');
-const Lor = require('./src/lor');
-const Lol = require('./src/lol');
-const Tft = require('./src/tft');
-
 module.exports = { Riot, Val, Lor, Lol, Tft };
