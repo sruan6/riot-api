b309e66f75e4644bc5e9d63e635d5428a973cfa9
ruan060606 <ruan060606@yahoo.com>
2020-10-24 01:54:15 -0700

    sec

--- src/lol/clashV1/index.js
+++ src/lol/clashV1/index.js
@@ -1,13 +1,60 @@
 const axios = require('axios');
+const Summoner = require('../summonerV4');
 
 // CLASH-V1
 const Clash = function Clash(region, apikey) {
-    this.playersByAccId = async (AccId) => {
-        const {data} = await axios.get(
-            `https://${region[0]}/lol/clash/v1/players/by-summoner/${AccId}?api_key=${apikey}`
-        );
-        return data;
-    };
-}
+  this.playersById = async (Id) => {
+    const { data } = await axios.get(
+      `https://${region[0]}/lol/clash/v1/players/by-summoner/${Id}?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.playersByAccId = async (AccId) => {
+    const summoner = await new Summoner(region, apikey);
+    const { id } = await summoner.summonerByAccId(AccId);
+    const { data } = await axios.get(
+      `https://${region[0]}/lol/clash/v1/players/by-summoner/${id}?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.playersByPuuid = async (Puuid) => {
+    const summoner = await new Summoner(region, apikey);
+    const { id } = await summoner.summonerByPuuid(Puuid);
+    const { data } = await axios.get(
+      `https://${region[0]}/lol/clash/v1/players/by-summoner/${id}?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.playersByName = async (name) => {
+    const summoner = await new Summoner(region, apikey);
+    const { id } = await summoner.summonerByName(name);
+    const { data } = await axios.get(
+      `https://${region[0]}/lol/clash/v1/players/by-summoner/${id}?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.teamsByTeamId = async (teamId) => {
+    const { data } = await axios.get(
+      `https://${region[0]}/lol/clash/v1/teams/${teamId}?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.tournaments = async () => {
+    const { data } = await axios.get(
+      `https://${region[0]}/lol/clash/v1/tournaments?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.tournamentsByTeam = async (teamId) => {
+    const { data } = await axios.get(
+      `https://${region[0]}/lol/clash/v1/tournaments/by-team/${teamId}?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.tournamentsByTournamentId = async (tournamentId) => {
+      const {data}= await axios.get(`https://${region[0]}/lol/clash/v1/tournaments/${tournamentId}?api_key=${apikey}`)
+      return data
+  }
+};
 
 module.exports = Clash;
