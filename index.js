f832073d2cda6176e03ac7d1989f9f841189f6d4
ruan060606 <ruan060606@yahoo.com>
2020-10-24 04:41:19 -0700

    index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// const Riot = require('./dist/riot');
-// const Val = require('./dist/val');
-// const Lor = require('./dist/lor');
-// const Lol = require('./dist/lol');
-// const Tft = require('./dist/tft');
+const Riot = require('./dist/riot');
+const Val = require('./dist/val');
+const Lor = require('./dist/lor');
+const Lol = require('./dist/lol');
+const Tft = require('./dist/tft');
 
 module.exports = { Riot, Val, Lor, Lol, Tft };
