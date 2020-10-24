0ba3ad132573466ac93aa48f514670788260e3da
ruan060606 <ruan060606@yahoo.com>
2020-10-24 01:28:45 -0700

    readme_update

--- src/val/matchV1/index.js
+++ src/val/matchV1/index.js
@@ -1,8 +1,23 @@
 const axios = require('axios');
 
-const Match = function Match() {
-  this.matchesByMatchId = async () => {
-    return 'Currently in development';
+const Match = function Match(region, apikey) {
+  this.matchesByMatchId = async (matchId) => {
+    const { data } = await axios.get(
+      `https://${region[0]}/val/match/v1/matches/${matchId}?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.matchlistsByPuuid = async (puuid) => {
+    const { data } = await axios.get(
+      `https//${region[0]}/val/match/v1/matchlists/by-puuid/${puuid}?api_key=${apikey}`,
+    );
+    return data;
+  };
+  this.recentMatchesByQueue = async (queue) => {
+    const { data } = await axios.get(
+      `https://${region[0]}/val/match/v1/recent-matches/by-queue/${queue}?api_key=${apikey}`,
+    );
+    return data;
   };
 };
 
